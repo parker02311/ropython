17021a7465ab21baf85ef867059ea4bf996a35ee
parker02311 <35582919+parker02311@users.noreply.github.com>
2021-09-24 10:34:30 -1000

    Start package

---
+++ robloxdevproduct/__init__.py
@@ -0,0 +1,6 @@
+__title__ = 'robloxdevproduct'
+__author__ = 'parker02311'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021 parker02311'
+
+from .client import Client
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="robloxdevproduct", # Replace with your own username
+    version="0.1.0",
+    author="parker02311",
+    author_email="parker02311contact@gmail.com",
+    description="A API wrapper for interacting with developer products in python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/parker02311/Custom-Developer-Products-Python/tree/master",
+    packages=['robloxdevproduct'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
