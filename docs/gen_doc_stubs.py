722226194f655678ab1516e032796b88f644cc66
parker02311 <35582919+parker02311@users.noreply.github.com>
2021-09-25 08:42:10 -1000

    Fix typeerror hopefully

--- docs/gen_doc_stubs.py
+++ docs/gen_doc_stubs.py
@@ -7,11 +7,11 @@ nav = mkdocs_gen_files.Nav()
 for path in sorted(Path("ropython").glob("**/*.py")):
     module_path = path.with_suffix("")
     doc_path = path.with_suffix(".md")
-    nav_path = doc_path.replace("ropython", "reference")
+    nav_path = str(doc_path).replace("ropython", "reference")
 
-    nav[module_path.parts] = str(doc_path)
-    print(doc_path)
-    with mkdocs_gen_files.open(doc_path, "w") as f:
+    nav[module_path.parts] = str(nav_path)
+    print(nav_path)
+    with mkdocs_gen_files.open(nav_path, "w") as f:
         ident = ".".join(module_path.parts)
         data = "::: " + ident
         print(data, file=f)
