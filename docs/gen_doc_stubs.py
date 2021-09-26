58d032dc336b898e42519125bc9db30dd52d03a3
parker02311 <35582919+parker02311@users.noreply.github.com>
2021-09-26 11:47:53 -1000

    Update gen_doc_stubs.py

--- docs/gen_doc_stubs.py
+++ docs/gen_doc_stubs.py
@@ -5,17 +5,16 @@ import mkdocs_gen_files
 nav = mkdocs_gen_files.Nav()
 
 for path in sorted(Path("ropython").glob("**/*.py")):
-    if not path == "ropython.__init__":
-        module_path = path.with_suffix("")
-        doc_path = path.with_suffix(".md")
-        nav_path = str(doc_path).replace("ropython", "reference")
+    module_path = path.with_suffix("")
+    doc_path = path.with_suffix(".md")
+    nav_path = str(doc_path).replace("ropython", "reference")
 
-        nav[module_path.parts] = str(nav_path)
-        print(nav_path)
-        with mkdocs_gen_files.open(nav_path, "w") as f:
-            ident = ".".join(module_path.parts)
-            data = "::: " + ident
-            print(data, file=f)
+    nav[module_path.parts] = str(nav_path)
+    print(nav_path)
+    with mkdocs_gen_files.open(nav_path, "w") as f:
+        ident = ".".join(module_path.parts)
+        data = "::: " + ident
+        print(data, file=f)
 
 # generated_nav = nav.build_literate_nav()
 
