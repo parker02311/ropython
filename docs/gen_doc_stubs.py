f026c53317b1c840b7d08f4d0a98873afa68f203
parker02311 <35582919+parker02311@users.noreply.github.com>
2021-12-01 09:40:59 -1000

    replace gen_docs_stubs to see if it fixes

--- docs/gen_doc_stubs.py
+++ docs/gen_doc_stubs.py
@@ -1,34 +1,35 @@
-#!/usr/bin/env python
-
 from pathlib import Path
 import mkdocs_gen_files
 nav = mkdocs_gen_files.Nav()
 
-for path in sorted(Path("ropython").glob("**/*.py")):
-    module_path = path.with_suffix("")
-    doc_path = path.with_suffix(".md")
-    nav_path = str(doc_path).replace("ropython", "reference")
+package_names = ["ropython"]
 
-    nav[module_path.parts] = str(nav_path)
-    print(nav_path)
-    with mkdocs_gen_files.open(nav_path, "w") as f:
-        ident = ".".join(module_path.parts)
-        data = "::: " + ident
-        print(data, file=f)
+for package_name in package_names:
+    for path in sorted(Path(package_name).glob("**/*.py")):
+        module_path = path.with_suffix("")
+        doc_path = path.with_suffix(".md")
 
-# generated_nav = nav.build_literate_nav()
+        full_doc_path = Path("reference", doc_path)
 
-# nav_string = ""
+        nav[module_path.parts] = str(doc_path)
+        print(full_doc_path)
+        with mkdocs_gen_files.open(full_doc_path, "w") as f:
+            ident = ".".join(module_path.parts)
+            data = "::: " + ident
+            print(data, file=f)
 
-# for nav_piece in generated_nav:
-#     nav_piece = nav_piece.replace("[\\__init__]", "[\\_\\_init\\_\\_]")
-#     nav_piece = nav_piece.replace("__init__.md", "/__init__.md")
-#     nav_string += nav_piece
+        # mkdocs_gen_files.set_edit_path(full_doc_path, path)
+
+generated_nav = nav.build_literate_nav()
+
+nav_string = ""
+
+for nav_piece in generated_nav:
+    # Fix bugs with __init__ files. Why does this work? No idea.
+    nav_piece = nav_piece.replace("[\\__init__]", "[\\_\\_init\\_\\_]")
+    nav_piece = nav_piece.replace("__init__.md", "/__init__.md")
+    nav_string += nav_piece
 
 
-# with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
-#     print(nav_string)
-#     nav_file.write(nav_string)
-
-# with mkdocs_gen_files.open("reference/SUMMARY.md", "r") as nav_file:
-#     print(nav_file.read())
+with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
+    nav_file.write(nav_string)
