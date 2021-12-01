0961254163a30291da1c85b4bdf17da839cfae34
parker02311 <35582919+parker02311@users.noreply.github.com>
2021-12-01 09:53:43 -1000

    Attempt to chang nav

--- docs/gen_doc_stubs.py
+++ docs/gen_doc_stubs.py
@@ -2,18 +2,15 @@ from pathlib import Path
 import mkdocs_gen_files
 nav = mkdocs_gen_files.Nav()
 
-package_names = ["ropython"]
 
-for package_name in package_names:
-    for path in sorted(Path(package_name).glob("**/*.py")):
-        module_path = path.with_suffix("")
-        doc_path = path.with_suffix(".md")
+for path in sorted(Path("ropython").glob("**/*.py")):
+    module_path = path.with_suffix("")
+    doc_path = path.with_suffix(".md")
+    nav_path = str(doc_path).replace("ropython", "reference")
 
-        full_doc_path = Path("reference", doc_path)
-
-        nav[module_path.parts] = str(doc_path)
-        print(full_doc_path)
-        with mkdocs_gen_files.open(full_doc_path, "w") as f:
-            ident = ".".join(module_path.parts)
-            data = "::: " + ident
-            print(data, file=f)
+    nav[module_path.parts] = str(nav_path)
+    print(nav_path)
+    with mkdocs_gen_files.open(nav_path, "w") as f:
+        ident = ".".join(module_path.parts)
+        data = "::: " + ident
+        print(data, file=f)
